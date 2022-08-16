92948d202b8732ef3371a27e6f0bf4ab186e74b7
HIMANSHU KUMAR SINGH <himanshusingh051998@gmail.com>
2022-08-16 20:40:35 +0530

    first commit

---
+++ 2/rules.js
@@ -0,0 +1,8 @@
+//.1
+
+//a) Use -> If any request executes then it will execute, its function.
+//b) Route -> If route matches then it will execute, its function.
+//c) get/put/post -> If request method matches then it will execute, its function.
+
+//2.
+
